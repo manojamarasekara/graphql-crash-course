2ade14aade9d4d11b4a38425227ec5dbfe33df92
p-maa <bmanoj.amarasekara@gmail.com>
2023-10-20 09:25:17 +0530

    Added resolver functions to the project

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone-server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// db
+import db from './_db.js';
 
 // types
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    reviews() {
+      return db.reviews
+    },
+    authors() {
+      return db.authors
+    }
+  }
+}
 
 const server = new ApolloServer({
   typeDefs,
--- schema.js
+++ schema.js
@@ -1,28 +1,22 @@
 export const typeDefs = `#graphql
   type Game {
-    id: ID!,
-    title: String!,
-    platform: [String!]!,
-    description: String,
-    releaseDate: String,
-    price: Int,
-    image: String,
+    id: ID!
+    title: String!
+    platform: [String!]!
   }
   type Review {
-    id: ID!,
-    rating: Int!,
-    content: String 
+    id: ID!
+    rating: Int!
+    content: String!
   }
   type Author {
-    id: ID!,
-    name: String!,
-    verified: Boolean!,
+    id: ID!
+    name: String!
+    verified: Boolean!
   }
   type Query {
-    reviews: [Review],
-    games: [Game],
-    author: [Author]
+    games: [Game]
+    reviews: [Review]
+    authors: [Author]
   }
 `
-
-// int, float, string, boolean, ID
